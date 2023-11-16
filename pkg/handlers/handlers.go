ccceeeb584a05bf6095737f897a398ee8ded4798
Eddy <edwardyue93@gmail.com>
2023-11-16 04:25:03 +0100

    Starting template

---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,49 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/eddyvy/go-bookings/pkg/config"
+	"github.com/eddyvy/go-bookings/pkg/models"
+	"github.com/eddyvy/go-bookings/pkg/render"
+)
+
+// Repo the repository used by the handlers
+var Repo *Repository
+
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIp := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIp)
+
+	render.RenderTemplate(w, "home.page.html", &models.TemplateData{})
+}
+
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	// perform some logic
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again"
+
+	remoteIp := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIp
+
+	// send the data to the template
+	render.RenderTemplate(w, "about.page.html", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
